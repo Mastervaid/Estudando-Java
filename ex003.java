233d3c3f1bc1c61fa96f75cc6bd62ff71b256263
Thiago Macedo <tm09529@gmail.com>
2024-03-14 17:51:55 -0300

    Create ex003.java

---
+++ ex003.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class ex003 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numeroA, numeroB, maiornumero=0, menornumero=0, sobra;
+        System.out.print("Digite o numero A: ");
+        numeroA = sc.nextInt();
+        System.err.print("digite o numero B: ");
+        numeroB = sc.nextInt();
+
+        if (numeroA > numeroB) {
+            maiornumero = numeroA;
+            menornumero = numeroB;
+        }else if (numeroB > numeroA){
+            maiornumero = numeroB;
+            menornumero = numeroA;
+        }else{
+            System.out.println("Os numero são iguas");
+        }
+        
+        sobra = maiornumero % menornumero;
+        if (sobra == 0) {
+            System.out.println("São multiplos");
+        }else{
+            System.out.println("Não são multiplos");
+        }
+    }
+}
