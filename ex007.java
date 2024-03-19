b434949fc0e4e54b18b5382abbcc54c57c549366
Thiago Macedo <tm09529@gmail.com>
2024-03-19 11:51:36 -0300

    ex007

---
+++ ex007.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class ex007 {
+   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double coodernadaX;
+        double coodernadaY;
+        System.out.print("Digite a coodernadaX: ");
+        coodernadaX = sc.nextDouble();
+        System.out.print("digite a coodernadaY: ");
+        coodernadaY = sc.nextDouble();
+
+        if (coodernadaX > 0) {
+            if (coodernadaY > 0) {
+                System.out.println("Q1");
+            }else if (coodernadaY < 0) {
+                System.out.println("Q4");
+            }
+        }else if (coodernadaX < 0) {
+            if (coodernadaY > 0) {
+                System.out.println("Q2");
+            }else if (coodernadaY < 0) {
+                System.out.println("Q3");
+            }            
+        }else{
+            System.out.println("origem");
+        }
+    }
+}
+
