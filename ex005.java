7b63a324371ccaf377df3dee2ceca3eabac976cb
Thiago Macedo <tm09529@gmail.com>
2024-03-18 21:18:01 -0300

    Create ex005.java

---
+++ ex005.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.*;
+public class ex005 {
+   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Map<Integer, Double> map = new HashMap<>();
+        int entrada1;
+        int entrada2;
+        map.put(1, 4.00);
+        map.put(2, 4.50);
+        map.put(3, 5.00);
+        map.put(4, 2.00);
+        map.put(5, 1.50);
+
+        System.err.println("Digite os codigo: ");
+        entrada1 = sc.nextInt();
+        entrada2 = sc.nextInt();
+
+        double total = (double)(map.get(entrada1) + map.get(entrada2));
+        System.err.printf("TOTAL: R$ %.2f", total);
+
+    }
+}
