1861e2939e179357ffc7dea90fc2c08bd11ecd3f
Thiago Macedo <tm09529@gmail.com>
2024-03-19 11:17:11 -0300

    6 exercicios de java

---
+++ ex006.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class ex006 {
+   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double numero;
+        System.out.println("Informe o numero: ");
+        numero = sc.nextDouble();
+        if (numero >= 0.00 && numero < 25.00) {
+            System.out.println("Intervalo [0,25]");
+        }else if (numero >= 25.00 && numero < 50.00) {
+            System.out.println("Intervalo [25,50]");            
+        }else if (numero >= 50.00 && numero < 75.00) {
+            System.out.println("Intervalo [50,75]");    
+        }else if (numero >= 75.00 && numero <= 100.00) {
+            System.out.println("Intervalo [75,100]");
+        }else{
+            System.out.println("Fora de intervalo");
+        }
+    }
+}
